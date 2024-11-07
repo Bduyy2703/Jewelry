e54a0346af75d9e14346c37d18bcad9a1cf3cf3f
Phong Phạm Hùng <hungphong.canhan@gmail.com>
2024-11-07 14:41:42 +0700

    User listing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes';
-import { DefaultLayout } from './components/Layout';
+import { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes";
+import { DefaultLayout } from "./components/Layout";
+
+import AdminLayout from "./components/Layout/AdminLayout/AdminLayout";
+import AdminUserList from "./pages/admin/user/AdminUserList";
+import AdminUserDetail from "./pages/admin/user/AdminUserDetail";
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Page = route.component;
 
-                        let Layout = DefaultLayout;
+                    let Layout = DefaultLayout;
 
-                        if (route.layout) {
-                            Layout = route.layout;
-                        } else if (route.layout === null) {
-                            Layout = Fragment;
-                        }
+                    if (route.layout) {
+                        Layout = route.layout;
+                    } else if (route.layout === null) {
+                        Layout = Fragment;
+                    }
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
+                })}
+                <Route path='/admin/' element={<AdminLayout />}>
+                    <Route index element={<AdminUserList />} />
+                    {/* ADMIN USER */}
+                    <Route path='/admin/user' element={<AdminUserList />} />
+                    <Route
+                        path='/admin/user/:id'
+                        element={<AdminUserDetail />}
+                    />
+                </Route>
+            </Routes>
         </Router>
     );
 }
